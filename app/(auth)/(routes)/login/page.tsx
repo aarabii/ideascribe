e5c9fc842b1a699b07e256e44c4bdaa7957daebb
losier <arv95464@gmail.com>
2024-05-29 22:53:38 +0530

    fixed title

--- app/(auth)/(routes)/login/page.tsx
+++ app/(auth)/(routes)/login/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Login | Ideascribe",
+  title: "Login",
   description: "Login to Ideascribe",
 };
 
--- app/(auth)/(routes)/register/page.tsx
+++ app/(auth)/(routes)/register/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Register | Ideascribe",
+  title: "Register",
   description: "Register to Ideascribe",
 };
 
